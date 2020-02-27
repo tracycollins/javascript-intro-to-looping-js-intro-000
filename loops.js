a6df23e52462fe707d6a489d2d2dc52ef952eb7f
Tracy Collins <tc@threeceemedia.com>
2020-02-27 06:10:47 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
   for(var i=0; i<25; i++){
-    const text = `I am ${i} strange loop${i<1?'':'s'}.`
+    const text = `I am ${i} strange loop${i<=1?'':'s'}.`
     array.push(text)
   }
   return array
